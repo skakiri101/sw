d528b5cd93e08b1a0bd3af2bf370f63a418e3a05
skakiri101 <83859356+skakiri101@users.noreply.github.com>
2023-04-04 13:14:11 +0900

    Add files via upload

---
+++ 202395014/0328/p115_6.py
@@ -0,0 +1,12 @@
+num1=25 #변수 num1에 47 배정
+num2=5 #변수 num2에 18 배정
+
+hap=num1+num2 #hap변수에 num1+num2 저장
+cha=num1-num2 #cha변수에 num1-num2 저장
+gob=num1*num2 #cha변수에 num1*num2 저장
+nan=num1/num2 #nan변수에 num1/num2 저장
+
+print("25과 5의 합은 : ",hap) #결과 출력
+print("25과 5의 차은 : ",cha) #결과 출력
+print("25과 5의 곱은 : ",gob) #결과 출력
+print("25과 5의 나눗셈은 : ",nan) #결과 출력
---
+++ 202395014/0404/0404ex1.py
@@ -0,0 +1,18 @@
+'''
+2023.04.04
+남태호
+표준출력(print)연습
+'''
+
+print('한국','korea',2023)
+print('한국','korea',2023,sep='-') #구분자(-)넣어서 출력
+print('한국','korea',2023,end='@') #문장끝 @출력
+print("\n") #한 줄 띄우기
+print('python','language');print("Hi python")
+print('python','language',end=' ');print("Hi python") #줄 바꿈 없이 출력
+print("Hi pthon")
+print("'Hi python'") #작은 따음표 출력
+print("\'Hi python\'") #작은 따음표 출력
+print("python "*5) #5번 반복 출력
+print("hi \n pyhon") #\n(줄바꿈) 출력
+print("\n 포멧 활용") #한줄 띄우고 문자 출력
