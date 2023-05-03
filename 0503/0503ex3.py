0210305e7e9b364c2aa57753fd45e7c2919548ba
skakiri101 <83859356+skakiri101@users.noreply.github.com>
2023-05-03 15:21:03 +0900

    Add files via upload

---
+++ 0503/0503ex3.py
@@ -0,0 +1,23 @@
+'''
+2023-05-03
+남태호
+    1~입력받은 숫자까지의 합계 구하기
+#문제분석
+    변수-숫자(num),합계(total)
+#알고리즘
+    1.변수선언
+        num에 정수 입력
+        total=0
+    2.논리(반복)
+        (조건) for i in range(1,num+1): 
+
+'''
+
+#for반복
+num=int(input("합계 구할 숫자 : "))
+total=0 #합계
+
+for i in range(1,num+1): #조건
+    total=total+i
+
+print('1~{}까지의 합계 구하기',total)
